f5b361f8c22a4ee463393fc6187d7491f8459459
bethanyhung <bethany_hung@brown.edu>
2017-11-18 16:31:29 -0500

    distance in visual angle

--- gaborDisplay.m
+++ gaborDisplay.m
@@ -1,15 +1,13 @@
 function [output] = gaborDisplay
-%   INPUT: 
-%   nTrials, optional
-%   Defaults to 5. 
 
 %%  TO-DO LIST
 %   1. Compute visual angle based on viewing distance & size of screen
-%   2. Make a trialInfo-like data structure. 
 %   3. Look up best timing parameters. 
 %   4. Reject keypresses during fixation-cross-only display.
 
 addpath(genpath('~/code/pac'));
+
+fixationDur = 1;
 
 %% USER INPUT
 subj = input('Subject name: ','s');
@@ -39,13 +37,14 @@ shuffledPairs = allPairs(randperm(size(allPairs,1)),:);
 
 %% GABOR PATCH PARAMETERS
 
-%%%%%%%%%%%%%%%%%%%%%%%
-% Stimulus parameters %
-%%%%%%%%%%%%%%%%%%%%%%%
+% DIMENSIONS IN VISUAL DEGREES
+gaborDiam = 2;
+
 % Pixel per º/vis ang
 ppd = 40.5;
 
-si = 50; % half the desired gabor size
+% si = 50; 
+si = ceil(ppd*(gaborDiam/2))+1; % half the desired gabor size in PIXELS
 
 % Size of support in pixels, derived from si:
 tw = 2*si;
@@ -68,12 +67,12 @@ KbQueueStart;  %starts the cue
 
 %% TRIALS
 KbName('UnifyKeyNames');
-activeKeys = [KbName('LeftArrow') KbName('RightArrow')];
+activeKeys = [KbName('LeftArrow') KbName('RightArrow') KbName('q')];
 t2wait = 2; % maximum time to wait for response
 RestrictKeysForKbCheck(activeKeys);
 ListenChar(2);
 grey = GrayIndex(win,0.5);
-
+HideCursor
 Screen(win, 'FillRect', grey);
 Screen('Flip', win); 
 Screen('TextSize', win, 20);
@@ -90,7 +89,6 @@ for i = 1:nTrials
         nGabors = 7;
     end
 
-    HideCursor
     [w, h] = RectSize(winRect);
     ifi = Screen('GetFlipInterval', win); % how necessary is this? no animations
     Screen('BlendFunction', win, GL_ONE, GL_ONE);
@@ -131,6 +129,10 @@ for i = 1:nTrials
     else
         rotAngles = [tilt repmat(flankerAngle,[1,2])+90];
     end
+    
+    % BLANK SCREEN ITI 
+    trialITI = rand+.5;
+    WaitSecs(trialITI) %jitters prestim interval between .5 and 1.5 seconds; inter-trial interval  
 
     % DRAW FIXATION CROSS
     fixCrossDimPix = 20; % size
@@ -139,8 +141,8 @@ for i = 1:nTrials
     allCoords = [xCoords; yCoords];
     lineWidthPix = 1; % linewidth
     Screen('DrawLines', win, allCoords,lineWidthPix, WhiteIndex(screenid), [w/2 h/2], 2);
-    WaitSecs(0.75)
     Screen('Flip',win);
+    WaitSecs(fixationDur)
       
     % DRAW GABOR PATCHES + FIXATION 
     Screen('DrawLines', win, allCoords,lineWidthPix, WhiteIndex(screenid), [w/2 h/2], 2);
@@ -154,32 +156,92 @@ for i = 1:nTrials
     else
         dstRects = CenterRectOnPointd(inrect .* repmat(scale,4,1), x, y);
     end
-    WaitSecs(rand+.5) %jitters prestim interval between .5 and 1.5 seconds; inter-trial interval  
-    vbl = Screen('Flip', win);
+
+    vbl = Screen('Flip', win);    
+    trialStart = tic;   
+    timedout = 1;
     
-    %%%%%%% START %%%%%%%
-    KbQueueFlush;
-    tStart = GetSecs;
-    timedout = false;
-    while ~timedout
-        [keyIsDown, keyTime, keyCode] = KbCheck; 
+    while toc(trialStart) < t2wait && timedout
+        [keyIsDown, keyTime, keyCode] = KbCheck;
         if keyIsDown
             Beeper;
-            break; 
-        end
-        if (keyTime - tStart) > t2wait
-            Beeper(900);
-            timedout = true; 
+            timedout = 0;
+            timeElapsed = toc(trialStart);
+        elseif keyIsDown && strcmp(key(1),'q')
+            showcursor;
+            ListenChar(0);
+            Screen('CloseAll');
+            return;
         end
     end
     
     if ~timedout
-        rsp.RT(i) = keyTime - tStart;
+        rsp.RT(i) = timeElapsed;
         rsp.keyName{i} = KbName(keyCode);
     else
         rsp.RT(i) = t2wait;
         rsp.keyName{i} = 'none';
     end
+%     
+%     if ~rspGiven
+%         Beeper(900);
+%         timedout = 1; 
+%     end
+%     
+%     
+%     stimulus_onset_time = tic;    
+%     response(i) = -1;
+%     % Loop to look for reach movements
+%     while (toc(stimulus_onset_time) < RTDeadline && ~exit)
+% 
+%         [keyIsDown, secs, keyCode]= KbCheck;
+%           key = KbName(keyCode);
+%             if ((keyIsDown)&& ((strcmp(key(1),'z')) || (strcmp(key(1),'x')) || (strcmp(key(1),'c')) || (strcmp(key(1),'v'))))
+%                timeElapsed = toc(stimulus_onset_time);  
+%                if strcmp(key(1),'z')
+%                    response(i) = 1;
+%                elseif  strcmp(key(1),'x')
+%                    response(i) = 2;
+%                elseif strcmp(key(1),'c')
+%                    response(i) = 3;
+%                elseif strcmp(key(1),'v')
+%                    response(i) = 4;
+%                end
+%                 exit = 1;
+%             elseif ((keyIsDown)&& strcmp(key(1),'q'))
+%                         showcursor;
+%                         ListenChar(0);
+%                         Screen('CloseAll');
+%                         return;
+%             end
+% 
+%      
+%     end
+%     
+%     %%%%%%% START %%%%%%%
+%     
+%     
+%     tStart = GetSecs;
+%     timedout = false;
+% %     while ~timedout
+% %         [keyIsDown, keyTime, keyCode] = KbCheck; 
+% %         if keyIsDown
+% %             Beeper;
+% %             break; 
+% %         end
+% %         if (keyTime - tStart) > t2wait
+% %             Beeper(900);
+% %             timedout = true; 
+% %         end
+% %     end
+%     
+%     if ~timedout
+%         rsp.RT(i) = keyTime - tStart;
+%         rsp.keyName{i} = KbName(keyCode);
+%     else
+%         rsp.RT(i) = t2wait;
+%         rsp.keyName{i} = 'none';
+%     end
     %%%%%%% END %%%%%%%
     vbl = Screen('Flip', win, vbl + 0.5 * ifi); 
     
